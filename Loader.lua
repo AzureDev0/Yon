6a0062117574362cf04bfaa3e5c134927bbb1448
ByteDev0 <atrenish2@yahoo.com>
2025-02-02 21:00:27 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -53,7 +53,7 @@ if Loader.Screen.Enabled == false then
     local gameId = game.GameId
 
     local GameIds = {
-        [3150475059] = "https://gist.githubusercontent.com/ByteDev0/0968bd469ef768dbfb68ab5462e8a096/raw/237ae0f411ecd22c3ffd424ecfd9f893b2977f1b/FootballFusion2.lua",
+        [3150475059] = "https://gist.githubusercontent.com/ByteDev0/0968bd469ef768dbfb68ab5462e8a096/raw/bcd10c5cd12a0c51c731792797f0bed509af1090/FootballFusion2.lua",
         [6325068386] = "https://gist.githubusercontent.com/ByteDev0/9c2785f927f00b21add41169850f8c30/raw/0851075d72d8ee7b0eec66d035197223bcbe04a1/BlueLockRivals.lua",
         [3808223175] = "https://gist.githubusercontent.com/ByteDev0/9dcc5d70f9727b34977c299330f3e571/raw/7a0a54ee1b63c66db9180f2bebf20af035971c4c/JujutsuInfinite.lua",
     }
